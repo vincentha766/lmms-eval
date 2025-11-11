1bd7017cff27e8d0dbacae677daa65183f591810
Vincent Wu <vincentha766@gmail.com>
2025-11-11 00:11:58 +0000

    Update test scripts

--- test/mmerealworld.sh
+++ test/mmerealworld.sh
@@ -3,5 +3,5 @@ accelerate launch \
 	-m lmms_eval \
 	--model qwen2_5_vl \
 	--model_args=pretrained=Qwen/Qwen2.5-VL-3B-Instruct,max_pixels=376320,interleave_visuals=False \
-	--tasks mme \
+	--tasks mmerealworld \
 	--batch_size 1
