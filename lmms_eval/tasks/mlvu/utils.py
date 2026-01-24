a314693d8f209edaf9f5f1b8233e339d654e70dc
Huanan Wu <vincentha766@gmail.com>
2026-01-24 10:21:02 +0800

    Fix default HF_HOME value typo in mlvu

--- lmms_eval/tasks/mlvu/utils.py
+++ lmms_eval/tasks/mlvu/utils.py
@@ -14,7 +14,7 @@ from loguru import logger as eval_logger
 
 from lmms_eval.tasks._task_utils.file_utils import generate_submission_file
 
-hf_home = os.getenv("HF_HOME", "./~/.cache/huggingface")
+hf_home = os.getenv("HF_HOME", "~/.cache/huggingface")
 # hf_home="/share/junjie/shuyan/lmms-eval/~/.cache/huggingface"
 base_cache_dir = os.path.expanduser(hf_home)
 
